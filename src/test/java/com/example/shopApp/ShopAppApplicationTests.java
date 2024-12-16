8b3f9b0f3118157b095c094a5b3e5067da71ecde
trangniii <nguyenthitrangyiiii@gmail.com>
2024-12-17 01:23:33 +0700

    init

---
+++ src/test/java/com/example/shopApp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
